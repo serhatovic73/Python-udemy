0054c7207f59c7fceba8bc02df6b824c27264499
Sironess <87657730+serhatovic73@users.noreply.github.com>
2026-01-27 10:51:51 +0100

    update

---
+++ 4Python_degiskenler_giris/degisken_kullaniminda_dikkat_edilmesi_gerekenler.py
@@ -0,0 +1,8 @@
+isim = "serhat" #degisken isimleri ingilizce karakterler olmalidir
+Isim = "rabia" #buyuk kucuk harf uyumu vardir
+Ogrenci_adi = "recep" #okunurluk acisindan _ kullanilir
+
+3sayi = "rakam" #bu sekilde calismaz sayi once gelemez
+
+-ak = 47 #bu sekilde de hata veri ozel karakter girmemeli
+print(-ak)
---
+++ 4Python_degiskenler_giris/degiskenler-giris.py
@@ -0,0 +1,20 @@
+from datetime import date #date fonksiyonunu import edelim
+#pythonda degisken türü otomatik belirlenir
+
+#degisken olusturduk
+isim = "Hakan "
+soyIsim = "Can"
+yas = 22
+dogumTarihi = date(2009,5,3)
+anneAdi = "Cemile"
+babaAdi = "Remzi"
+
+print("Merhaba " + isim + "Hosgeldiniz! ")
+
+print("Kimlik Verileriniz :"
+       "\nAd : " + isim +
+       "\nSoyad : " + soyIsim + 
+        "\nYas : " + str(yas) +
+        "\nDogum Tarihi : " + str(dogumTarihi) +
+        "\nAnne Adi : " + anneAdi +
+        "\nBaba Adi : "  + babaAdi)
