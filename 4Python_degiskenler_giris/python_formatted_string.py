4005a8646b4b0c24773d22a40e19931f3d54e788
Sironess <87657730+serhatovic73@users.noreply.github.com>
2026-01-27 21:14:57 +0100

    Update

---
+++ 4Python_degiskenler_giris/python_formatted_string.py
@@ -0,0 +1,7 @@
+#asagida da gozuktugu gibi print(f"merhaba {isim} gibi + felan kullanmadan rahatca yazilabilir ")
+isim = "Ece"
+print(f"Merhaba {isim} Hosgeldiniz!")
+print(f"Sayin {isim} asagidaki islemlerden birini seciniz : ")
+
+
+
